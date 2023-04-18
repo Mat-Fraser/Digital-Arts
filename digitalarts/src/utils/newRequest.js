5df4756fe9b579cb4608d0341e891b37032d962b
Mat-Fraser <86045021+Mat-Fraser@users.noreply.github.com>
2023-04-18 13:26:26 +0530

    Update newRequest.js

--- digitalarts/src/utils/newRequest.js
+++ digitalarts/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://digital-arts.onrender.com/",
+  baseURL: "https://digital-arts.onrender.com",
   withCredentials: true,
 });
 
